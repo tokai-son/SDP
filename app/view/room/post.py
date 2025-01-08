028ec7821f175ba13b4e2959802e0075a992f66a
Ryo Ajima <oh.girl627@gmail.com>
2025-01-08 22:30:14 +0900

    fix

--- app/view/room/post.py
+++ app/view/room/post.py
@@ -1,14 +1,17 @@
+import logging
 from typing import Dict
 
 def view_request(request : Dict) -> Dict:
     name = request.get("name")
-    if name is None:
+    if name is None or name == "":
+        logging.error("name is required")
         raise ValueError("name is required")
 
     return {"name": name}
 
 def view_response(response : Dict) -> Dict:
     roomID = response.get("roomID")
-    if roomID is None:
+    if roomID is None or roomID == "":
+        logging.error("roomID is required")
         raise ValueError("webSocketURL is required")
     return {"roomID": roomID}
